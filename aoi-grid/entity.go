90cbe39f4b613c4fe969f0b4eb30204a7b9e905a
dyc92 <552058945@qq.com>
2021-06-09 19:10:15 +0800

    add entity

---
+++ aoi-grid/entity.go
@@ -0,0 +1,38 @@
+﻿package aoigrid
+
+import (
+	. "aoi/common"
+)
+
+type Entity struct {
+	ID        int
+	GridID    int
+	SightGrid int
+	KnowGrid  int
+	Name      string
+	Type      EntityType
+
+	Position    Vector  //位置
+	Rotation    Vector  //旋转
+	Orientation Vector  //方向
+	MaxSpeed    float32 //最大速度
+
+	worldBox, sightBox, knownBox A3DAABB
+
+	collisionRadius float32 //碰撞半径
+	KnownRadius     float32
+	SightRadius     float32
+
+	HideSelf  bool
+	HideOther bool
+}
+
+func (e *Entity) init() {
+	e.KnownRadius = 0.5
+	e.SightRadius = 20.0
+	e.MaxSpeed = 15.0
+	e.worldBox = NewBox()
+	e.sightBox = NewBox()
+	e.knownBox = NewBox()
+	e.collisionRadius = 0.1
+}
---
+++ common/aabb.go
@@ -0,0 +1,29 @@
+﻿package common
+
+//轴对齐碰撞箱
+type A3DAABB struct {
+	Center, //坐标中心
+	Extents, //线段 箱形体边长
+	Mins,
+	Maxs Vector
+}
+
+func NewBox() A3DAABB {
+	box := A3DAABB{}
+	box.Clear()
+	return box
+}
+
+func (a *A3DAABB) Clear() {
+	a.Maxs.Set(999999.0, 999999.0, 999999.0)
+	a.Maxs.Set(-999999.0, -999999.0, -999999.0)
+}
+
+func (a *A3DAABB) CompleteCenterExts() {
+	a.Center = a.Mins.Add(a.Maxs).Mul(0.5)
+	a.Extents = a.Maxs.Sub(a.Center)
+}
+
+func (a *A3DAABB) IsValid() bool {
+	return a.Mins.X <= a.Maxs.X && a.Mins.Y <= a.Maxs.Y && a.Mins.Z <= a.Maxs.Z
+}
